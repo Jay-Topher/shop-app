5fcd41992fa99324b8ee81ed1cf48a572719797e
Jones Ogolo <jay.topher11@gmail.com>
2020-06-16 17:03:51 +0100

    changed header font style

--- navigation/shopNavigation.js
+++ navigation/shopNavigation.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import ProductsOverviewScreen from "../screens/Shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/Shop/ProductDetailScreen";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import Colors from "../constants/Colors";
+import HeaderButton from "../components/UI/HeaderButton";
+import CartScreen from "../screens/Shop/CartScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,9 +19,32 @@ function ProductStack() {
         headerStyle: {
           backgroundColor: Colors.primary,
         },
+        headerTitleStyle: {
+          fontFamily: "raleway-bold",
+        },
+        headerBackTitleStyle: {
+          fontFamily: "raleway",
+        },
       }}
     >
-      <Stack.Screen name="Products" component={ProductsOverviewScreen} />
+      <Stack.Screen
+        name="Products"
+        component={ProductsOverviewScreen}
+        options={({ navigation }) => ({
+          headerTitle: "All Products",
+          headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+              <Item
+                title="Cart"
+                iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+                onPress={() => {
+                  navigation.navigate("Cart");
+                }}
+              />
+            </HeaderButtons>
+          ),
+        })}
+      />
       <Stack.Screen
         name="ProductDetail"
         component={ProductDetailScreen}
@@ -26,6 +54,7 @@ function ProductStack() {
           };
         }}
       />
+      <Stack.Screen name="Cart" component={CartScreen} />
     </Stack.Navigator>
   );
 }

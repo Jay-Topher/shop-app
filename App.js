26015de255a0068f86fe8c6200b57c328b2f172e
Jones Ogolo <jay.topher11@gmail.com>
2020-06-16 17:02:02 +0100

    added fonts to app

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { createStore, combineReducers } from "redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-import ShopNavigator from "./navigation/shopNavigation";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
+import ShopNavigator from "./navigation/shopNavigation";
 import productsReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
 });
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    raleway: require("./assets/Fonts/Raleway-Regular.ttf"),
+    "raleway-bold": require("./assets/Fonts/Raleway-SemiBold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <NavigationContainer>
